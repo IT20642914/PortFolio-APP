cc1feae54b55df4ce32dd64b6c2539ab14bf128f
IT20642914 <avishkachanaka@gmail.com>
2024-05-03 03:00:33 +0530

    xxx

---
+++ backend/models/Portfolio.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+const postSchema = new mongoose.Schema({
+  portfolio_name: {
+    type: String,
+    require: true,
+  },
+  bio: {
+    type: String,
+    require: true,
+  },
+  description: {
+    type: String,
+    require: true,
+  },
+  category: {
+    type: String,
+    require: true,
+  },
+  email: {
+    type: String,
+    require: true,
+  },
+  contact_no: {
+    type: String,
+    require: true,
+  },
+  image: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Image',
+  },
+  gallery: [
+    {
+      title: String,
+      description: String,
+      image: String,
+    },
+  ],
+});
+
+module.exports = mongoose.model('Post', postSchema);
