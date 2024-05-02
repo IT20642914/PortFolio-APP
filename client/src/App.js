c935f62cdfd5a8c974ee3bdb6054423a84d58256
IT20642914 <avishkachanaka@gmail.com>
2024-05-03 03:17:54 +0530

    media content frontend

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,13 @@ import Login from "./pages/Login/Login";
 import SignUp from "./pages/SignUp/SignUp";
 import SideNavBar from "./components/SideNav/SideNavBar";
 import UserManagement from "./pages/UserManagement/UserManagement";
+
+import Dashboard from './components/ContentDashboard';
+import Add from './components/Add';
+import MediaList from './components/MediaList';
+import UpdateContent from './components/UpdateContent';
+import SingleMedia from "./components/viewone";
+
 function App() {
 
   const ADminLayoutRoutes = ({ children }) => (
@@ -22,7 +29,16 @@ function App() {
       <Route path="/"  name="Login" element={<Login />} /> 
       <Route path="/signup"  name="SignUp" element={<SignUp />} />
       <Route path="/jobs" name="jobs" element={<Jobs />} />
-      {/* Admin routes */}
+
+            {/* mediaRoutes */}
+             <Route path="/media" element={<Dashboard />} />
+            <Route path="/media/view" element={<MediaList />} />
+            <Route path="/media/add" element={<Add />} />
+            <Route path="/media/update/:id" element={<UpdateContent />} />
+            <Route path="/media/get/:id" element={<SingleMedia />} />
+
+
+            {/* Admin routes */}
            <Route path="/adminJobs" name="adminJobs" element={<ADminLayoutRoutes><CareerAdmin/></ADminLayoutRoutes>}/>
            <Route path="/UserManagement" name="adminJobs" element={<ADminLayoutRoutes><UserManagement/></ADminLayoutRoutes>}/>
 
